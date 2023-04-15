6a9e11c978d4abed7219593636a98cb9dc791f1d
bo1234566 <wangbohpu@163.com>
2023-04-15 23:39:55 +0800

    base function of discusspost and AJAX display

---
+++ src/test/java/com/nowcoder/community/SensitiveTests.java
@@ -0,0 +1,25 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.SensitiveFilter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class SensitiveTests {
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitiveFilter() {
+        String text = "@主*席不可以台)独，哈哈哈";
+        String result = sensitiveFilter.filter(text);
+        System.out.println(result);
+    }
+
+}

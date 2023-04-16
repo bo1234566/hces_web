a4abac07462e91040079c42bbe2b04a0e25773de
bo1234566 <wangbohpu@163.com>
2023-04-16 23:32:12 +0800

    base function of message &  AJAX logic

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -150,4 +150,8 @@ public class UserService implements CommunityConstant {
         return userMapper.updateHeader(userId, headerUrl);
     }
 
+    public User findUserByName(String username) {
+        return userMapper.selectByName(username);
+    }
+
 }

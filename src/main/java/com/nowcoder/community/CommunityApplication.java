b6d7f96dfdb8804616fcc927a9b5e0e2bb093332
bo1234566 <wangbohpu@163.com>
2023-04-26 21:01:19 +0800

    base function of elasticsearch and show on html

--- src/main/java/com/nowcoder/community/CommunityApplication.java
+++ src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -5,8 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class CommunityApplication extends SpringBootServletInitializer  {
+	@PostConstruct
+	public void init() {
+		// 解决netty启动冲突问题
+		// see Netty4Utils.setAvailableProcessors()
+		System.setProperty("es.set.netty.runtime.available.processors", "false");
+	}
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(CommunityApplication.class);
